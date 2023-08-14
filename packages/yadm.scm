8d43c58d757e3c279ee77d92d7d7d5152336558f
lucascool12 <lucas.van.laer@gmail.com>
2023-08-14 09:45:21 +0100

    init

---
+++ packages/yadm.scm
@@ -0,0 +1,23 @@
+(define-module (yadm)
+	       #:use-module ((guix licenses) #:prefix license:)
+	       #:use-module (guix packages)
+	       #:use-module (guix download)
+	       #:use-module (guix utils)
+	       #:use-module (guix build-system gnu))
+
+(define-public yadm
+  (package
+    (name "yadm")
+    (version "3.2.2")
+    (source (origin
+	      (method url-fetch)
+	      (uri (string-append
+		     "https://github.com/TheLocehiliosan/" name "/archive/refs/tags/" version ".tar.gz"))
+	      (sha256
+		(base32
+		  "1b9k9izi9kxwicqsa06gdh8q1mkad3dbrn11ib0f8p4r923m1yy5"))))
+    (build-system gnu-build-system)
+    (synopsis "yadm")
+    (description "yadm")
+    (home-page "https://yadm.io/")
+    (license license:gpl3+)))
